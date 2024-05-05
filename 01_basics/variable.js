9a2021e02beae625f52aaead39b8412871e36812
Sajedullah Aref <152142322+rf104@users.noreply.github.com>
2024-05-05 02:36:33 +0000

    Variable File Add!!

---
+++ 01_basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 144452
+let accountName = "Aref"
+var accountPass = "1243"
+accountCity  = "Dhaka"
+var accoutnState;
+
+console.log(accountId);
+
+accountName = "Sajedullah Aref"
+accountPass = "0192384"
+accountCity = "Chittagong"
+
+/*
+Prefer not to use var 
+because of issue in block scope & functionality scope!
+Use Let Instead of Var!
+*/
+
+console.table([accountId,accountName,accountPass,accountCity,accoutnState]);
