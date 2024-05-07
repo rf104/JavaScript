e1942cbfaf79805ca46c2b4f00dc2b25e21f6cfb
rf_104 <kingaref13@gmail.com>
2024-05-07 22:17:53 +0600

    modified array folder

---
+++ 02_basics/array.js
@@ -0,0 +1,12 @@
+///Arrays
+
+let myArray = [12,3,4,5,56,23] /// its 0 indexing 
+console.log(myArray[0]); /// 12
+let newArray = new Array(12,4,5,6,4,5,5,5); // Another way of declering an array
+console.log(newArray[2]); /// 5
+
+let arr = [1, "aref", 23, 'a']; // multiple datatypes can be added in a single array. Even Object,(array itself ) also added to the array. 
+
+console.log(arr[1]); ///aref
+console.log(arr[2]); ///23
+console.log(arr[3]); ///a
