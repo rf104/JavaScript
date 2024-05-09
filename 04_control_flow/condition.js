76140cfa57b8ed1ed152092a0bad66ac19317852
rf_104 <kingaref13@gmail.com>
2024-05-09 16:54:43 +0600

    control_flow add

---
+++ 04_control_flow/condition.js
@@ -0,0 +1,11 @@
+let balance = 1000;
+
+if(balance < 500){
+    console.log("less than 500");
+}else if(balance < 750){
+    console.log("less than 750")
+}else if(balance < 900){
+    console.log("less than 900");
+}else{
+    console.log("less than 1200");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        console.log("April!");
+        break;
+}
