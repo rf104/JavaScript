63aaf9469997d330c270a51181798cdb37020fc3
rf_104 <kingaref13@gmail.com>
2024-05-20 21:31:49 +0600

    while loop added

---
+++ 05_iteration/two.js
@@ -0,0 +1,21 @@
+///While Loop
+
+let index = 1;
+while(index<=10){
+    console.log(`Values of index is ${index}`);
+    index++
+}
+
+let arr = ['Aref', 'Hafsa', 'Tawsif'];
+
+let indx = 0;
+while(indx<arr.length)
+    {
+        console.log(`Value of Index ${indx} is ${arr[indx]}`);
+        indx++
+    } 
+
+    let score = 11;
+    do{
+        console.log(`Score is ${score}`); // in do-while at first make the change and then it check condition of while loop!!
+    }while(score<=10)
