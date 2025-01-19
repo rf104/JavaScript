8967d95f498c6858f9c4fe862a3cf2a0eac87a1f
rf_104 <kingaref13@gmail.com>
2025-01-19 08:39:10 +0600

    practice_1 modified!!

--- 01_basics/practice_1.js
+++ 01_basics/practice_1.js
@@ -1,3 +1,51 @@
-let url = "https://sajedulla_aref_103.com";
+let arr1 = ['aref','taqi','tawsif'];
+let arr2 = ['hafsa','Riad','Hasib'];
 
-console.log(url.replace('103','204'));
+let arr3 = arr1.concat(arr2);
+//console.log(arr3);
+
+let isLogedIn = ""
+let booleanIsLogedIn = Boolean(isLogedIn);
+//console.log(booleanIsLogedIn);
+
+let someNumber = 22;
+let convertString = String(someNumber);
+//console.log(convertString[0]);
+
+let x = 4;
+let y = x++;
+//console.log(`value of x is ${x} and Value of y is ${y}`);
+let a = 3;
+let b = ++a;
+
+// console.log(`value of a is ${a} and Value of b is ${b}`);
+
+// let userMail = "sajedullah_aref_104@yahoo.com";
+// let user2Mail = userMail;
+
+// user2Mail = "kingaref104@gmail.com";
+// console.log(user2Mail);
+// console.log(userMail);
+
+let userOne = {
+    mail:"sajedullah_aref_104@yahoo.com",
+    age: 22
+}
+let user2 = userOne;
+
+user2.mail = "tawsif@gmail.com";
+
+// console.log(user2.mail);
+// console.log(userOne.mail);
+
+
+let Name = "  aref 104 "
+// console.log(Name);
+// console.log(Name.trim());
+
+let score = 400;
+console.log(score); //400
+
+let balance = new Number(100)
+console.log(balance); //[Number: 100]
+
